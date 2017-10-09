f79a187cd0b23a1597fc510b3a37afba221accb8
Hans Höchtl <hhoechtl@1drop.de>
2017-10-09 09:02:13 +0200

    [TASK] introduce privilegeTarget for download restriction, add signal

--- Classes/Slot/FileAccess.php
+++ Classes/Slot/FileAccess.php
@@ -5,11 +5,12 @@ namespace Onedrop\RestrictedFiles\Slot;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Request as HttpRequest;
 use Neos\Flow\Mvc\ActionRequest;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 use Neos\Flow\ResourceManagement\PersistentResource;
-use Neos\Flow\Http\Request as HttpRequest;
 use Neos\Flow\Security\Account;
+use Neos\Flow\Security\Authorization\PrivilegeManagerInterface;
 use Neos\Flow\Security\Context;
 use Neos\Flow\Security\Exception\AccessDeniedException;
 use Onedrop\RestrictedFiles\Domain\Model\DownloadCount;
@@ -32,10 +33,15 @@ class FileAccess
      */
     protected $entityManager;
     /**
-     * @Flow\InjectConfiguration(path="trackProtectedDownloads")
+     * @Flow\InjectConfiguration(path="trackProtectedDownloadsByAccount")
      * @var boolean
      */
-    protected $trackProtectedDownloads;
+    protected $trackProtectedDownloadsByAccount;
+    /**
+     * @var PrivilegeManagerInterface
+     * @Flow\Inject
+     */
+    protected $privilegeManager;
 
     /**
      * @param PersistentResource $resource
@@ -49,17 +55,38 @@ class FileAccess
         /** @noinspection PhpMethodParametersCountMismatchInspection */
         $actionRequest = $this->objectManager->get(ActionRequest::class, $httpRequest);
         $this->securityContext->setRequest($actionRequest);
-        // todo: check with settings
-        if (false && !$this->securityContext->hasRole('Neos.Neos:Editor')) {
+        // Deny access to the requested file if permission is missing
+        if (!$this->privilegeManager->isPrivilegeTargetGranted('Onedrop.RestrictedFiles:Download')) {
+            $this->emitDownloadDenied($resource, $httpRequest);
             throw new AccessDeniedException(
                 sprintf('You are not allowed to access the file %s', $resource->getFilename()),
                 1485716879
             );
-        } elseif ($this->trackProtectedDownloads && $this->securityContext->getAccount() instanceof Account) {
+        }
+        // Track download if configured and user is authenticated
+        if ($this->trackProtectedDownloadsByAccount && $this->securityContext->getAccount() instanceof Account) {
             $downloadCount = new DownloadCount($resource, $this->securityContext->getAccount());
             $this->entityManager->persist($downloadCount);
             $this->entityManager->flush();
         }
+    }
+
+    /**
+     * This is just a dummy method to have a methodPrivilege to check the permission
+     */
+    public function download()
+    {
+
+    }
+
+    /**
+     * @Flow\Signal
+     * @param PersistentResource $resource
+     * @param HttpRequest        $httpRequest
+     * @return void
+     */
+    protected function emitDownloadDenied(PersistentResource $resource, HttpRequest $httpRequest)
+    {
     }
 
 }
--- Readme.md
+++ Readme.md
@@ -18,11 +18,77 @@ resources themselves.
 You should create an AssetCollection in the Neos backend
 and configure the title of that collection to be protected:
 
-    Onedrop:
-      RestrictedFiles:
-        collectionNames: ['Protected']
+```yaml
+Onedrop:
+  RestrictedFiles:
+    collectionNames: ['Protected']
+```
 
+## Restrict download to permission
+
+This package also provides a simple way to restrict the download of a 
+file to a `privilegeTarget` that can be assigned to any role.
+
+To grant access to the files just add this privilegeTarget to a role:
+```yaml
+roles:
+  'Some.Package:SomeUser':
+    privileges:
+      -
+        privilegeTarget: 'Onedrop.RestrictedFiles:Download'
+        permission: GRANT
+```
+
+You can disable this feature by setting:
+```yaml
+roles:
+  'Neos.Flow:Everybody':
+    privileges:
+      -
+        privilegeTarget: 'Onedrop.RestrictedFiles:Download'
+        permission: GRANT
+```
+
+## Handling unauthorized download attempts
+
+This package will emit a `\Neos\Flow\Security\Exception\AccessDeniedException` if 
+an unauthorized access to a download occurs. There is a signal you can 
+subscribe on to change that behavior to e.g. redirect to a page.
+
+Example:
+
+```php
+<?php 
+
+public function redirectToReferer(PersistentResource $resource, HttpRequest $httpRequest)
+{
+    $referer = $httpRequest->getHeader('Referer');
+    if (!empty($referer)) {
+        $refererUri = new Uri($referer);
+        if ($refererUri->getHost() === $httpRequest->getUri()->getHost()) {
+            $refererUri->setQuery('--restricted-files[accessDenied]=true');
+            header('Location: ' . $refererUri->__toString());
+            exit();
+        }
+    }
+}
+```
+
+## Download tracking
+
+By default this package tracks every download of an authenticated account.
+
+You can disable this feature by setting:
+```yaml
+Onedrop:
+  RestrictedFiles:
+    trackProtectedDownloadsByAccount: false
+```
+
+## How it works
 
 It uses doctrine lifecycle hooks to copy resources between 
 Flow resource collections if an asset is moved into a protected
 asset collection in the neos backend (or uploaded of course).
+
+To check the access it uses the signals emitted by the PrivateResources package.
